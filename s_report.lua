c45b45db6aeebb3b6303d9bbf10dfde3fb9f5621
Logan Cypser <loganmatthewcyp@gmail.com>
2019-07-07 22:07:57 -0400

    Update s_report.lua
    Added ability to ping user

--- s_report.lua
+++ s_report.lua
@@ -1,6 +1,7 @@
 webHookURL = "" -- The Webhook link to post too.
 local DISCORD_NAME = "Report Bot" -- Changes the name of the bot (default : Report Bot)
 local DISCORD_IMAGE = "https://www.realbookies.com/wp-content/uploads/2019/03/reporting-analytics_60.png" -- Any Image URL works
+local role = "" -- Must be a role id
 enableReport = true -- Enable /report command
 
 -- Note, the command has to start with `/`.
@@ -9,13 +10,15 @@ TriggerEvent('chat:addSuggestion', '/report', 'Reports A Player To Staff', {
     { name="Reason", help="param description 2" }
 })
 
+local role = "<@&"..role..">"
+
 if enableReport then
     RegisterCommand("report", function(source, args, rawCommand)
         local user = tonumber(args[1])
         local msg = table.concat(args, " ", 2)
         local username = GetPlayerName(user)
 		local reporter = GetPlayerName(source)
-		
+		print(user)
 		if username == nil then
 			print("Please make sure to fill in all fields")
 		else
@@ -37,5 +40,5 @@ function sendMessage(username, msg, color, reporter)
             },
         }
     }
-	PerformHttpRequest(webHookURL, function(err, text, headers) end, 'POST', json.encode({username = DISCORD_NAME, embeds = connect, avatar_url = DISCORD_IMAGE}), { ['Content-Type'] = 'application/json' })
+	PerformHttpRequest(webHookURL, function(err, text, headers) end, 'POST', json.encode({username = DISCORD_NAME, content = role , embeds = connect, avatar_url = DISCORD_IMAGE}), { ['Content-Type'] = 'application/json' })
 end
